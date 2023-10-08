46e62583b1b4592a2f39559c891e4cc4f7855c5d
zariiii9003 <zariiii9003@users.noreply.github.com>
2023-10-08 20:52:54 +0200

    try to build pypy wheels

--- setup.py
+++ setup.py
@@ -1,39 +1,17 @@
 #!/usr/bin/env python
-
-import sys
-import setuptools
 import platform
 
-
-def is_cpython_3():
-    if platform.python_implementation() != "CPython":
-        return False
-
-    if sys.version_info[0] < 3:
-        return False
-
-    return True
+import setuptools
 
 
-def setup(ext_modules):
+if platform.python_implementation() in ("CPython", "PyPy"):
+    ext_modules = [
+        setuptools.Extension(
+            "bitstruct.c",
+            sources=["src/bitstruct/c.c", "src/bitstruct/bitstream.c"],
+        )
+    ]
+
     setuptools.setup(
         ext_modules=ext_modules,
     )
-
-
-if is_cpython_3():
-    try:
-        setup(
-            [
-                setuptools.Extension(
-                    "bitstruct.c",
-                    sources=["src/bitstruct/c.c", "src/bitstruct/bitstream.c"],
-                )
-            ]
-        )
-    except:
-        print("WARNING: Failed to build the C extension.")
-        setup([])
-else:
-    print("INFO: C extension only implemented in CPython 3.")
-    setup([])
